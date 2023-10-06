6346520260e1f820380d364144b5d6a27624c555
sooyeon.jang <tn7278@naver.com>
2023-10-07 02:22:42 +0900

    Webpack

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
-// import plus from './plus.js';
 import './style.css';
-import rabbit from './rabbit.png';
+import bg from './background.png';
+
+let env;
+
+if (process.env.NODE_ENV === 'development') {
+    env = dev;
+} else {
+    env = pro;
+}
+
+console.log(env);
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.body.innerHTML = `<img src="${rabbit}"/>`;
+    document.body.innerHTML = `<img src="${bg}"/>`;
 })
